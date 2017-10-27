2d71cb6918acaf69c3109e4c5e1aa11a6afa874f
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-10-27 11:06:40 +0900

    basic firebase push functionality

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function firebaseInit() {
   // Initialize Firebase
   // Get this code from the google firebase console
@@ -9,24 +11,35 @@ function firebaseInit() {
     storageBucket: "web-test-project-3049e.appspot.com",
     messagingSenderId: "620721885462"
   };
+  // Log firebase to see there are no errors console.log(firebase)
   firebase.initializeApp(config);
-  // Log firebase to see there are no errors
-  console.log(firebase);
 
   // create a firebase database object
   var database = firebase.database();
-  console.log(database);
+  var ref = database.ref('scores');
+  return ref;
 }
 
 function gameInit() {
   var button = document.getElementById('button');
   var score = document.getElementById('score');
+  var save = document.getElementById('save');
   var value = 0;
+  var firebase = firebaseInit();
 
   button.onclick = addScore;
+  save.onclick = saveScore;
 
   function addScore(e) {
     value += 1;
     return score.innerHTML = value;
+  }
+
+  function saveScore(e) {
+    var data = {
+      name: "Adum",
+      score: value,
+    };
+    firebase.push(data);
   }
 }
